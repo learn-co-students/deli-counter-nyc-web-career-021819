2271c0e2288538bc15d0b379e443f7625b9803ef
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 16:21:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+deli =[]
+
+def line(deli)
+  message= "The line is currently:"
+  if deli.length >0 
+  deli.each.with_index(1) do |name, index|
+    message +=" #{index}. #{name}"
+  end
+  puts message
+  else 
+puts "The line is currently empty."
+end
+end 
+
+def take_a_number(deli,name)
+  deli.push(name)
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
+end
+
+def now_serving(deli)
+  if deli.length ==0
+    puts "There is nobody waiting to be served!"
+  else 
+  puts "Currently serving " + deli[0] + "."
+  deli.shift
+end
+end
